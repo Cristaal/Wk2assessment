1b0a784c31c11714981546e641fbfdc66d00f483
Crystal Contreras-Grossman <crystalcontrerasgrossman@gmail.com>
2015-01-20 21:03:58 -0800

    add additional specs

--- spec/phone_spec.rb
+++ spec/phone_spec.rb
@@ -4,12 +4,33 @@ require('phone')
 require('pry')
 
 describe(Phone) do
-  
+
+  before() do
+    Phone.clear()
+  end
+
   describe(".all") do
     it("is empty at first") do
       expect(Phone.all()).to(eq([]))
     end
   end
+
+  describe(".clear") do
+    it("clears out the phone array") do
+      test_number = Phone.new({:number_type => "work"})
+      test_number.save()
+      Phone.clear()
+      expect(Phone.all()).to(eq([]))
+    end
+  end
+
+  describe("#id") do
+    it("returns the id of the phone number type.") do
+      test_number = Phone.new({:number_type => "cell"})
+      expect(test_number.id()).to(eq(1))
+    end
+  end
+
 
   describe('#save') do
     it("saves phone number types to the class variable array") do
@@ -23,6 +44,7 @@ describe(Phone) do
   describe('#number_type') do
     it("returns the type of phone number") do
       test_number = Phone.new({:number_type => "work"})
+      test_number.save()
       expect(test_number.number_type()).to(eq("work"))
     end
   end
